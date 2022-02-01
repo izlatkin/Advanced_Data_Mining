54e55e53070804e849f06c70a6ef607066e12439
izlatkin <zlatkin_ilya@mail.ru>
2022-01-31 23:30:20 -0500

    config commit

--- adm.h
+++ adm.h
@@ -1,6 +1,9 @@
 //
 // Created by Ilya Zlatkin on 31.01.2022.
 //
+//data_mining
+#ifndef DATA_MINING_ADM_H
+#define DATA_MINING_ADM_H
 
 #include <cctype>
 #include <string>
@@ -14,3 +17,4 @@ typedef struct {
 } student;
 
 
+#endif
